80bb029110d0ad9fa7ab77a01361bbf13a9108c9
Surya Dasari <suryadasari0@gmail.com>
2026-03-07 11:53:58 +0000

    new changes

--- scripts/build-dotnet.sh
+++ scripts/build-dotnet.sh
@@ -5,9 +5,15 @@ echo "Building .NET service"
 
 cd src/cartservice
 
-dotnet restore
-dotnet build
+# restore dependencies
+dotnet restore cartservice.csproj
 
-VERSION=$(git rev-parse --short HEAD)
+# build project
+dotnet build cartservice.csproj --configuration Release
 
-cp bin/Debug/net8.0/cartservice.dll cartservice-${BUILD_NUMBER}-${VERSION}.dll
+VERSION=${BUILD_NUMBER}-$(git rev-parse --short HEAD)
+
+# copy artifact
+cp bin/Release/net8.0/cartservice.dll cartservice-${VERSION}.dll
+
+echo ".NET build completed"
